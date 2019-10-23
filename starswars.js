4d112f01f02e435a5f67ea0ea67fe0608c6f2378
Gavin T. Murphy <gavin.murphy@centre.edu>
2019-10-23 10:15:58 -0400

    10/23 start

---
+++ starswars.js
@@ -0,0 +1,29 @@
+var starPromise = d3.json("https://ghibliapi.herokuapp.com/films");
+var printTitles = function(starData)
+{
+d3.select(".titles")
+.append("ol")
+.selectAll("li")
+.data(starData)
+.enter()
+.append("li")
+.text(function(d) { return d.title})
+.on("click", function(d) { printData(d) });
+}
+var printData = function(movie)
+{
+d3.select(".data *").remove("ol");
+d3.select(".data").append("div").attr("class", "info");
+d3.select(".info").append("ol").attr("class", "infoList");
+d3.select(".infoList").append("li").text(movie.producer);
+d3.select(".infoList").append("li").text(movie.director);    
+}
+starPromise.then(
+function(starData)
+{
+printTitles(starData);
+console.log("starData", movieData);
+},
+);
+
+
