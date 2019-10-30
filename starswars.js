94fb32861ee7b5c3a66009051d9b3256d85f6fc3
Gavin T. Murphy <gavin.murphy@centre.edu>
2019-10-30 14:52:59 -0400

    2:52 10/30

--- starswars.js
+++ starswars.js
@@ -1,6 +1,6 @@
 var starPromise = d3.json("https://swapi.co/api/films/?format=json")
 var printTitles = function(starsData)
-{                                                                    //starData.results[0].title
+{                                                                    
 d3.select(".titles")
 .append("ol")
 .selectAll("li")
@@ -8,39 +8,60 @@ d3.select(".titles")
 .enter()
 .append("li")
 .text(function(d) { return d.title})
-.on("click", function(d) { printData(d) });
+.on("click", function(d) { console.log(d) 
+                          printData(d) });
+    
 }
-var printData = function(film)
-{ console.log(film.characters);
- 
- var chars = film.characters;
- console.log(chars);
- 
- var charPromise = chars.map(function(chars)
-        {
-                 console.log(charPromise);
-                 return d3.json(chars);
-}   )
-                
-Promise.all(promises).then(printData(function(values)
- {
-     console.log("values",values);
-     d3.select()
-     
- }))
 
-d3.select(".data *").remove("ul");
+
+var printData = function(film)
+{
+    console.log(film);
+    var chars = film.characters;
+    console.log(film,chars);
+    
+    
+    d3.select(".data *").remove("ul");
 d3.select(".data").append("div").attr("class", "info");
 d3.select(".info").append("ul").attr("class", "infoList");
-d3.select(".infoList").append("li").text(film.release_date);
-d3.select(".infoList").append("li").text(film.characters); 
-d3.select(".infoList").append("li").text(film.director);
-d3.select(".infoList").append("li").text(film.producer);
 d3.select(".infoList").append("li").text(film.opening_crawl);
+d3.select(".infoList").append("li").text(film.release_date);
+d3.select(".infoList").append("li").text("Characters", film.characters); 
+d3.select(".infoList").append("li").text("Director: " + film.director);
+d3.select(".infoList").append("li").text("Producer(s): " + film.producer);
+    
+    var charPromise = chars.map(function(chars)
+    {
+                 console.log(chars);
+                 return d3.json(chars);
+    })
+                
+    Promise.all(charPromise).then(function(values)
+    {
+    console.log(values[0].name);
+    console.log(values[5].name);
+     console.log("values",values);
+     d3.select("ul li:nth-child(3)")
+        .on("click", function(d) { console.log(d) 
+                          printData(d) })
+     
+        .append("ul")
+        .selectAll("li")
+        .data(values)
+        .enter()
+        .append("li")
+        .text(function(d) { return d.name})
+
+    })
+
+
+
 
 
 
 }
+
+
 starPromise.then(
 function(starData)
     
